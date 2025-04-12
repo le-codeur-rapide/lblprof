24242e81df8139017b82fad7a5e63b28d2cef982
paul vezia <pauvezia@publicisgroupe.net>
2025-04-12 19:44:25 +0200

    add display line of code being executed

--- fibonacci_settrace.py
+++ fibonacci_settrace.py
@@ -14,12 +14,25 @@ def show_trace(frame, event, arg):
     frame.f_trace_opcodes = True
     code = frame.f_code
     offset = frame.f_lasti
+    
+    # Get the source code of the current file
+    source_lines = None
+    try:
+        with open(code.co_filename, 'r') as f:
+            source_lines = f.readlines()
+    except:
+        source_line = "<source not available>"
+    
+    # Get the current line of code being executed
+    if source_lines and 0 <= frame.f_lineno-1 < len(source_lines):
+        source_line = source_lines[frame.f_lineno-1].strip()
+    else:
+        source_line = "<line not available>"
 
     print(f"| {event:10} | {str(arg):>4} |", end=' ')
     print(f"{frame.f_lineno:>4} | {frame.f_lasti:>6} |", end=' ')
-    print(f"{opcode.opname[code.co_code[offset]]:<18} | {str(frame.f_locals):<35} |")
+    print(f"{opcode.opname[code.co_code[offset]]:<18} | {str(frame.f_locals):<35} | {source_line}")
     return show_trace
-
 import sys
 
 header = f"| {'event':10} | {'arg':>4} | line | offset | {'opcode':^18} | {'locals':^35} |"
